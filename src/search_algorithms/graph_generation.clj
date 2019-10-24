e30518a5ba564f9074bb60d29058b579711890e1
SamHammersley <q5315908@live.tees.ac.uk>
2019-10-24 08:22:25 +0100

    Change random graph generation

--- src/search_algorithms/graph_generation.clj
+++ src/search_algorithms/graph_generation.clj
@@ -1,27 +1,23 @@
 (ns search-algorithms.graph-generation)
 
+(defn update-edges [graph origin-node destination-node cost]
+  (println origin-node destination-node cost)
+  (update (update graph origin-node conj {destination-node cost}) destination-node conj {origin-node cost}))
+
 (defn in? [coll e]
   (some #(= e %) coll))
 
-(defn random-element [coll existing]
-  (if (nil? existing)
-    (random-element coll '())
-    (let [available (remove #(in? existing %) coll)]
-      (if (nil? (first available)) nil (nth available (rand-int (count available)))))))
+(defn available-nodes [graph current-node]
+  (remove #(or (= % current-node) (in? (keys (graph current-node)) %)) (keys graph)))
 
-(defn update-edges [graph origin-node selected-node cost]
-  (update (update graph origin-node conj {selected-node cost}) selected-node conj {origin-node cost}))
-
-(defn generate-edges [origin-node graph min-cost max-cost]
-  (loop [n (inc (rand-int (- (count graph) (count (graph origin-node))))) edges graph]
-    (if (zero? n)
-      edges
-      (let [filtered-nodes (remove #(= % origin-node) (keys edges))
-            selected-node (random-element filtered-nodes (keys (edges origin-node)))
-            cost (+ min-cost (rand-int (- max-cost min-cost)))]
-        (if (nil? selected-node)
-          edges
-          (recur (- n (inc (rand-int (dec n)))) (update-edges edges origin-node selected-node cost)))))))
+(defn generate-edges [current-node graph min-cost max-cost probability]
+  (loop [nodes (available-nodes graph current-node), edges graph, p (rand)]
+     (if (nil? (first nodes))
+       edges
+       (let [cost (+ min-cost (rand-int (- max-cost min-cost)))
+             new-graph (if (< p probability) (update-edges edges current-node (first nodes) cost) edges)]
+         (recur (rest nodes) new-graph (rand))))
+     ))
 
 (def alphabet-start-index 97)
 
@@ -32,9 +28,13 @@
         nodes
         (recur (conj nodes {keyword {}}) (inc current-node-index))))))
 
-(defn generate-graph [n min-cost max-cost]
+(defn generate-graph [n min-cost max-cost probability]
   (loop [graph (generate-nodes n) nodes (keys graph)]
     (let [current-node (first nodes)]
       (if (nil? current-node)
         graph
-        (recur (conj graph (generate-edges current-node graph min-cost max-cost)) (rest nodes))))))
+        (recur (conj graph (generate-edges current-node graph min-cost max-cost probability)) (rest nodes))))))
+
+(defn min-probability-bound [n] (/ (java.lang.Math/log n) n))
+
+(defn generate-hopefully-connected-graph [n min-cost max-cost] (generate-graph n min-cost max-cost (min-probability-bound n)))
