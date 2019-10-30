2173e76ab3d6931cff666aefdadfd209ba1ef167
SamHammersley <q5315908@live.tees.ac.uk>
2019-10-30 11:17:20 +0000

    Add check if goal isn't in the nodes (goal is disconnected from graph)

--- src/search_algorithms/new_graph_generation.clj
+++ src/search_algorithms/new_graph_generation.clj
@@ -1,4 +1,5 @@
-(ns search-algorithms.new-graph-generation)
+(ns search-algorithms.new-graph-generation
+  (:use [search-algorithms.helper-functions]))
 
 (def alphabet-start-index 97)
 
@@ -6,9 +7,9 @@
   (let [cost (+ min-cost (rand-int (- max-cost min-cost)))]
     (loop [current-node (first other-nodes) nodes (rest other-nodes) p (rand) result {}]
       (if (nil? current-node)
-          result
-          (let [r (if (< p probability-threshold) (assoc result (str origin-node current-node) cost) result)]
-            (recur (first nodes) (rest nodes) (rand) r))
+        result
+        (let [r (if (< p probability-threshold) (assoc result (str origin-node current-node) cost) result)]
+          (recur (first nodes) (rest nodes) (rand) r))
         ))))
 
 (defn generate-new-edges [n min-cost max-cost probability-threshold]
@@ -21,14 +22,15 @@
         (let [e (generate-edge-for-each-node node (remove #(= % node) nodes) min-cost max-cost probability-threshold)]
           (recur (rest remaining) (first remaining) (conj edges e)))))))
 
-(generate-new-edges 11 1 4 0.5)
-
 (defn min-probability-threshold [n] (/ (java.lang.Math/log n) n))
 
 (defn generate-heuristics [goal search-algorithm distance-fn neighbours-fn]
-    (loop [nodes (keys neighbours-fn)
-           heuristics {}]
-      (println 'from (first nodes) 'to goal)
-      (if (empty? nodes)
-        heuristics
-        (recur (rest nodes) (assoc heuristics (first nodes) (last (search-algorithm (first nodes) goal distance-fn 0 neighbours-fn)))))))
+  (let [nodes (keys neighbours-fn)]
+    (if (not (in? nodes goal))
+      (println goal "is not in" nodes "something has gone horribly wrong!")
+      (loop [node (first nodes)
+             remaining-nodes (rest nodes)
+             heuristics {}]
+        (if (nil? node)
+          heuristics
+          (recur (first remaining-nodes) (rest remaining-nodes) (assoc heuristics node (last (search-algorithm node goal distance-fn 0 neighbours-fn)))))))))
